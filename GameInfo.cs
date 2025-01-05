4bc151d395576bd4c52592b9ed41577884936a9b
karbis <102873508+karbis@users.noreply.github.com>
2025-01-05 19:53:27 +0200

    game icons

---
+++ GameInfo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace robloxrpc {
+	internal class GameInfo: IDisposable {
+		public string Creator;
+		public string IconLink;
+		public string GameName;
+		public bool Invalidated = false;
+		public delegate void OnGameInfoRecieved();
+		public event OnGameInfoRecieved GameInfoRecieved;
+		HttpClient httpClient;
+		public long PlaceId;
+		public bool Ready = false;
+
+		public GameInfo(long placeId) {
+			httpClient = new HttpClient();
+			PlaceId = placeId;
+
+			Task.Run(getInfo);
+		}
+
+		private const string VERIFIED_BADGE = " ✔";
+		private void getInfo() {
+			bool isLocalFile = PlaceId == 0;
+			if (isLocalFile) {
+				GameName = "Local File";
+			} else {
+				GameDetails details = GetAsync<GameDetails>($"https://economy.roblox.com/v2/assets/{PlaceId}/details");
+				if (details == null) return;
+				GameName = details.Name;
+				Creator = $"{(details.Creator.CreatorType == CreatorType.User ? "@" : "")}{details.Creator.Name}{(details.Creator.HasVerifiedBadge ? VERIFIED_BADGE : "")}";
+			}
+
+			long gameId = (isLocalFile) ? 95206881 : PlaceId;
+			IconDetails icon = GetAsync<IconDetails>($"https://thumbnails.roblox.com/v1/places/gameicons?placeIds={gameId}&format=Png&size=256x256");
+			if (icon == null || Invalidated) return;
+			IconLink = icon.data[0].imageUrl;
+			Ready = true;
+			GameInfoRecieved?.Invoke();
+		}
+
+		public T GetAsync<T>(string url) {
+			if (Invalidated) return default(T);
+			try {
+				using HttpResponseMessage message = httpClient.GetAsync(url).Result;
+				string json = message.Content.ReadAsStringAsync().Result;
+				return JsonConvert.DeserializeObject<T>(json);
+			} catch {
+				return default(T);
+			}
+		}
+
+		public void Dispose() {
+			if (Invalidated) return;
+			Invalidated = true;
+			httpClient.Dispose();
+
+			foreach(OnGameInfoRecieved e in GameInfoRecieved.GetInvocationList()) {
+				GameInfoRecieved -= e;
+			}
+		}
+	}
+
+	public enum CreatorType {
+		User,
+		Group
+	}
+	class GameDetails {
+		public class CreatorData {
+			public string Name;
+			public bool HasVerifiedBadge;
+			public CreatorType CreatorType = CreatorType.User;
+		}
+		public string Name;
+		public CreatorData Creator;
+	}
+
+	class IconDetails {
+		public class Icon {
+			public string imageUrl;
+		}
+		public List<Icon> data;
+	}
+}
--- Program.cs
+++ Program.cs
@@ -9,148 +9,164 @@ using Newtonsoft.Json;
 using Timer = System.Timers.Timer;
 
 #pragma warning disable CA1416 // Validate platform compatibility
-namespace bruhshot {
+namespace robloxrpc {
 	static class Program {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main() {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new MyCustomApplicationContext());
-        }
-    }
+			Application.Run(new MyCustomApplicationContext());
+		}
+	}
 
+	public class MyCustomApplicationContext : ApplicationContext {
+		private NotifyIcon trayIcon;
+		static DiscordRpcClient client;
+		static string userId;
+		static GameInfo cachedGameInfo;
 
-    public class MyCustomApplicationContext : ApplicationContext {
-        private NotifyIcon trayIcon;
-        static DiscordRpcClient client;
-        static string userId;
-
-        public MyCustomApplicationContext() {
+		public MyCustomApplicationContext() {
 			// Initialize Tray Icon
 
 			var contextMenu = new ContextMenuStrip();
-            ToolStripMenuItem titleThingy = new ToolStripMenuItem("Roblox Studio RPC", null, null, "Roblox Studio RPC");
-            contextMenu.Items.Add(titleThingy);
-            contextMenu.Items.Add(new ToolStripSeparator());
-            var exitButton = new ToolStripMenuItem();
-            contextMenu.Items.AddRange(new ToolStripItem[] { exitButton });
-            exitButton.Text = "Exit";
-            exitButton.Click += Exit;
+			ToolStripMenuItem titleThingy = new ToolStripMenuItem("Roblox Studio RPC", null, null, "Roblox Studio RPC");
+			contextMenu.Items.Add(titleThingy);
+			contextMenu.Items.Add(new ToolStripSeparator());
+			var exitButton = new ToolStripMenuItem();
+			contextMenu.Items.AddRange(new ToolStripItem[] { exitButton });
+			exitButton.Text = "Exit";
+			exitButton.Click += Exit;
 
 			trayIcon = new NotifyIcon() {
-                Icon = Resources.AppIcon,
-                ContextMenuStrip = contextMenu,
-                Visible = true
-            };
+				Icon = Resources.AppIcon,
+				ContextMenuStrip = contextMenu,
+				Visible = true
+			};
 
 			userId = GetUserId();
-            Timer timer = new Timer();
-            timer.Interval = 1000;
-            timer.AutoReset = true;
-            timer.Start();
-            timer.Elapsed += (_, _2) => {
-                Update();
+			Timer timer = new Timer();
+			timer.Interval = 1000;
+			timer.AutoReset = true;
+			timer.Start();
+			timer.Elapsed += (_, _) => {
+				Update();
 			};
-        }
-        public static string GetUserId() {
+		}
+		public static string GetUserId() {
 			// since the installedplugins is located in a folder thats named by your user id,
 			// we need to go inside of the account switcher data and get the account that has the lowest user id
 			// as that is the one you are probably logged into
 
-            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Roblox\RobloxStudio\LoggedInUsersStore\https:\www.roblox.com")) {
-                string value = ((string)key.GetValue("users")).Replace("};{",",");
-                while (value.Substring(value.Length-1, 1) != "}") {
-                    value = value.Substring(0, value.Length - 1);
-                }
+			using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Roblox\RobloxStudio\LoggedInUsersStore\https:\www.roblox.com")) {
+				string value = ((string)key.GetValue("users")).Replace("};{",",");
+				while (value.Substring(value.Length-1, 1) != "}") {
+					value = value.Substring(0, value.Length - 1);
+				}
 				Dictionary<string, dynamic> users = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(value);
-                List<long> indices = [];
-                foreach (KeyValuePair<string, dynamic> pair in users) {
-                    indices.Add(Convert.ToInt64(pair.Key));
-                }
-                long userId = 9999999999999999;
-                foreach (long id in indices) {
-                    userId = Math.Min(id, userId);
-                }
+				List<long> indices = [];
+				foreach (KeyValuePair<string, dynamic> pair in users) {
+					indices.Add(Convert.ToInt64(pair.Key));
+				}
+				long userId = 9999999999999999;
+				foreach (long id in indices) {
+					userId = Math.Min(id, userId);
+				}
 				return userId.ToString();
-            }
-        }
-
-        enum Status {
-            NotRunning,
-            Active,
-            NoScript
-        }
-        class RpcData {
-            public Status Status = Status.Active;
-            public long Lines = 0;
-            public byte Type = 0;
-            public string Name = "";
-        }
-        public static void Update() {
-            string path = $@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\{userId}\InstalledPlugins\0\settings.json";
-            if (!File.Exists(path)) return;
-            string contents = "";
-            try {
-                contents = File.ReadAllText(path);
-            } catch {
-                return;
-            }
-
-            Dictionary<string, dynamic> settings = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(contents);
-            if (!settings.ContainsKey("RpcData")) return;
-            RpcData data = settings["RpcData"].ToObject<RpcData>();
-
-            if (data.Status == Status.NotRunning) {
-                if (client == null) return;
-                client.Dispose();
-                client = null;
-                return;
-            }
-
-            if (client == null) {
-                client = new DiscordRpcClient("1109820127605686273");
-                client.Initialize();
-			}
-            
-            if (data.Status == Status.NoScript) {
-                UpdatePresence($"Editing {data.Name}", null);
-            } else if (data.Status == Status.Active) {
-                string smallAssetToolTip = (data.Type == 0) ? "Script" : (data.Type == 1) ? "LocalScript" : (data.Type == 2) ? "ModuleScript" : "";
-				UpdatePresence($"Editing {data.Name}", $"{data.Lines} lines", $"script{data.Type}", smallAssetToolTip);
 			}
 		}
 
-        public static void UpdatePresence(string details, string state, string smallAssetName = null, string smallAssetToolTip = null) {
-            RichPresence presence = client.CurrentPresence?.Clone() ?? new RichPresence();
-            if (!presence.HasTimestamps()) {
-                presence.Timestamps = new Timestamps() { Start = DateTime.UtcNow };
+		enum Status {
+			NotRunning,
+			Active,
+			NoScript
+		}
+		class RpcData {
+			public Status Status = Status.Active;
+			public long Lines;
+			public byte Type;
+			public string Name;
+			public long PlaceId;
+		}
+		public static void Update() {
+			string path = $@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\{userId}\InstalledPlugins\0\settings.json";
+			if (!File.Exists(path)) return;
+			string contents = "";
+			try {
+				contents = File.ReadAllText(path);
+			} catch {
+				return;
 			}
-            if (!presence.HasAssets()) {
-                presence.Assets = new Assets() {
+
+			Dictionary<string, dynamic> settings = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(contents);
+			if (!settings.ContainsKey("RpcData")) return;
+			RpcData data = settings["RpcData"].ToObject<RpcData>();
+
+			if (data.Status == Status.NotRunning) {
+				if (cachedGameInfo != null) {
+					cachedGameInfo.Dispose();
+					cachedGameInfo = null;
+				}
+
+				if (client == null) return;
+				client.Dispose();
+				client = null;
+				return;
+			}
+
+			if (client == null) {
+				client = new DiscordRpcClient("1109820127605686273");
+				client.Initialize();
+			}
+			
+			if (data.Status == Status.NoScript) {
+				if (cachedGameInfo == null) {
+					cachedGameInfo = new GameInfo(data.PlaceId);
+					cachedGameInfo.GameInfoRecieved += Update;
+					UpdatePresence($"Editing", null);
+				} else if (!cachedGameInfo.Ready) {
+					UpdatePresence($"Editing", null);
+				} else {
+					string creator = (cachedGameInfo.Creator == null) ? null : $"By {cachedGameInfo.Creator}";
+					string smallAssetToolTip = (data.PlaceId == 0) ? null : cachedGameInfo.GameName;
+					UpdatePresence($"Editing {cachedGameInfo.GameName}", creator, cachedGameInfo.IconLink, smallAssetToolTip);
+				}
+			} else if (data.Status == Status.Active) {
+				string smallAssetToolTip = (data.Type == 0) ? "Script" : (data.Type == 1) ? "LocalScript" : (data.Type == 2) ? "ModuleScript" : "";
+				UpdatePresence($"Editing {data.Name}", $"{data.Lines} lines", $"scriptnewer{data.Type}", smallAssetToolTip);
+			}
+		}
+
+		public static void UpdatePresence(string details, string state, string smallAssetName = null, string smallAssetToolTip = null) {
+			RichPresence presence = client.CurrentPresence?.Clone() ?? new RichPresence();
+			if (!presence.HasTimestamps()) {
+				presence.Timestamps = new Timestamps() { Start = DateTime.UtcNow };
+			}
+			if (!presence.HasAssets()) {
+				presence.Assets = new Assets() {
 					LargeImageKey = "logo3",
 					LargeImageText = "Roblox Studio"
 				};
-            }
+			}
 
 			if (presence.State == state && presence.Details == details && (presence.Assets.SmallImageKey == smallAssetName)) return;
 			presence.State = state;
-            presence.Details = details;
-            presence.Assets.SmallImageKey = smallAssetName;
-            presence.Assets.SmallImageText = smallAssetToolTip;
-            client.SetPresence(presence);
+			presence.Details = details;
+			presence.Assets.SmallImageKey = smallAssetName;
+			presence.Assets.SmallImageText = smallAssetToolTip;
+			client.SetPresence(presence);
 		}
 
-        void Exit(object sender, EventArgs e) {
-            // Hide tray icon, otherwise it will remain shown until user mouses over it
-            trayIcon.Visible = false;
-            if (client != null) {
-                client.Dispose();
-            }
-            Application.Exit();
-        }
-    }
+		void Exit(object sender, EventArgs e) {
+			// Hide tray icon, otherwise it will remain shown until user mouses over it
+			trayIcon.Visible = false;
+			if (client != null) {
+				client.Dispose();
+			}
+			Application.Exit();
+		}
+	}
 }
