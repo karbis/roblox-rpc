07603ac1ec4b76ee35244ae60bc77bdb00f68b7f
karbis <102873508+karbis@users.noreply.github.com>
2025-01-05 21:08:12 +0200

    oops again

--- Program.cs
+++ Program.cs
@@ -92,7 +92,7 @@ namespace robloxrpc {
 			public long PlaceId;
 			public bool Playtesting;
 		}
-		static string[] typeToolTips = ["Script","LocalScript","ModuleScript","[Disabled] Script","[Disabled] LocalScript","[Disabled] ModuleScript"];
+		static string[] typeToolTips = ["Script","LocalScript","ModuleScript","[Disabled] Script","[Disabled] LocalScript"];
 		public static void Update() {
 			string path = $@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\{userId}\InstalledPlugins\0\settings.json";
 			if (!File.Exists(path)) return;
