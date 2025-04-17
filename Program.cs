82858ab416738bc273619b99798358ebd9c3d8bd
karbis <102873508+karbis@users.noreply.github.com>
2025-04-17 19:35:00 +0300

    logo change again again

--- Program.cs
+++ Program.cs
@@ -169,11 +169,11 @@ namespace robloxrpc {
 			}
 			if (!presence.HasAssets()) {
 				presence.Assets = new Assets() {
-					LargeImageKey = "logo4",
+					LargeImageKey = "logo5",
 					LargeImageText = "Roblox Studio"
 				};
 			} else {
-				presence.Assets.LargeImageKey = "logo4";
+				presence.Assets.LargeImageKey = "logo5";
 				presence.Assets.LargeImageText = "Roblox Studio";
 			}
 
@@ -187,7 +187,7 @@ namespace robloxrpc {
 				presence.Assets = new Assets() {
 					LargeImageKey = smallAssetName,
 					LargeImageText = smallAssetToolTip,
-					SmallImageKey = "logo4",
+					SmallImageKey = "logo5",
 					SmallImageText = "Roblox Studio",
 				};
 			}
